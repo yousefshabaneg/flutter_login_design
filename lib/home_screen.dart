c4fdc2081d7c3a65b503072e2654951681354d9b
Yousef Shaban <yousefshaban.eg@gmail.com>
2021-10-07 14:38:16 +0200

    Initial Commit.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,16 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  var MyText;
   @override
   Widget build(BuildContext context) {
-    MyText = "Hello World!";
     return Scaffold(
       appBar: AppBar(
         leading: Icon(
           Icons.menu,
         ),
-        title: Text(this.MyText),
         actions: [
           IconButton(
               onPressed: () {
