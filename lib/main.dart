75be648a0c8f4d7fcad11754936c3d899e8cceb9
Yousef Shaban <yousefshaban.eg@gmail.com>
2021-10-08 13:32:36 +0200

    Initial Commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:first_flutter_app/home_screen.dart';
+import 'package:first_flutter_app/login_screen.dart';
 
 void main() {
   runApp(MyApp());
